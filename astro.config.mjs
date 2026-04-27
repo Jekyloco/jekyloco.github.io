05b6fa17c30475bb613e63c8107416fcf149ffca
Jekyloco <251600267@qq.com>
2026-04-27 19:14:17 +0630

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://www.runawayshame.com',
+	site: 'https://jekyloco.github.io',
 	devToolbar: { enabled: false },
 	integrations: [mdx(), sitemap()],
 	fonts: [
